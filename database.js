5efdc86b0f424f9ce7598748bb93244e13d01112
Your Name <shalitha.charith97@gmail.com>
2022-07-10 20:20:20 +0530

    First project commit

---
+++ database.js
@@ -0,0 +1,8 @@
+const Pool = require("mysql/lib/Pool");
+
+module.exports.databaseOptions = {
+    host: "localhost",
+    database: " ammachchi_canteen_db ",
+    user: "root",
+    password: "Chari123",
+  };
