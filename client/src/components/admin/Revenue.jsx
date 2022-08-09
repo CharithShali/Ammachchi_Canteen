dc441175b5c1ebbb79d5045c26e854d3accf898b
Gobee Selvaratnam <gobeeshan@hyperslab.com>
2022-08-10 01:56:22 +0530

    revenue

---
+++ client/src/components/admin/Revenue.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import styled from "../tables/Check.module.css";
+import Heading from "../header/Heading";
+import Background from "../helpers/Background";
+import darkGreyBg from "../../images/dark-grey-bg.png";
+import axios from 'axios';
+
+class Revenue extends Component {
+
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          data:[],
+        }
+      }
+
+    componentDidMount(){
+        axios.get('http://localhost:3001/api/admin/income')
+        .then(
+            response=> {
+                if (response.status === 200) {
+                    this.setState({
+                      data:response.data,
+                    });
+            }
+      })
+    }
+
+    render() {
+    const text = (
+        <>
+            Total <span>Revenue</span>
+        </>
+    );
+    
+    const order = this.state.data.map((item) => {
+        return(
+            <tr key={item.id} className={styled.tr}>
+            <th className={styled.columns} >{item.name}</th>
+            <td className={styled.columns1} >{item.revenue}</td>
+            </tr>
+        );
+    });
+
+    return(
+        <Background url={darkGreyBg}>
+        <section id="feedback" className={styled.intro1}>
+        <Heading text={text} className="heading-md" />
+        <div className={styled.div} >
+            <table className={styled.table}>
+                <thead className="table-dark text-center" >
+                <tr className={styled.tr1}>
+                    <th scope="col" style={{textAlign: 'center'}} className={styled.firstRow}>Seller</th>
+                    <th scope="col" style={{textAlign: 'center'}} className={styled.firstRow}>Date</th>
+                </tr>
+                </thead>
+                <tbody>
+                    {order}    
+                </tbody>
+            </table>
+        </div>
+        </section>
+        </Background>
+    )
+    }
+};
+
+export default Revenue;
